50daa5f0f53977be91de2f56254ac170e08d58e0
m1ngjp <it.m1ngjp@gmail.com>
2024-10-21 23:34:00 +0900

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { doc, getDoc, setDoc } from "firebase/firestore";
 import { FiLoader } from "react-icons/fi";
 import Image from "next/image";
 import Link from "next/link";
+import { FaEnvelope } from "react-icons/fa";
 
 export default function Home() {
   const [noteID, setNoteID] = useState("");
@@ -56,40 +57,49 @@ export default function Home() {
 
   return (
     <>
-    <div className="md:w-full md:max-w-md mx-4 md:mx-auto overflow-hidden">
-      <div className="mt-16 mb-8 flex flex-col items-center">
-        <h1 className="text-5xl font-bold">Annoote</h1>
-        <div className="mt-2 flex">
-          <Link target="blank" rel="noopener noreferrer" href="/legal?tab=terms" className="outline-none hover:underline focus-visible:underline">Legal</Link>
-          <p className="mx-2">/</p>
-          <Link rel="noopener noreferrer" href="/contact" className="outline-none hover:underline focus-visible:underline">Contact</Link>
-          <p className="mx-2">/</p>
-          <Link target="blank" href="https://github.com/m1ngjp/annoote" rel="noopener noreferrer" className="outline-none hover:underline focus:underline">GitHub</Link>
+    <div className="md:w-3/4 mx-4 md:mx-auto">
+      <div className="py-4 flex items-center">
+        <div>
+          <Image src="/logo.svg" alt="logo" width={100} height={100} className="w-8" />
+        </div>
+        <div className="ml-auto flex items-center space-x-4">
+          <Link href="#">Link1</Link>
+          <Link href="#">Link1</Link>
+          <Link href="#" className="bg-secondary text-white px-4 py-2 rounded-full flex items-center shadow outline-none focus-visible:ring-2 focus-visible:ring-primary duration-200 ring-offset-2"><FaEnvelope className="mr-2" />Contact</Link>
         </div>
       </div>
-      <div className="mb-8 flex flex-col items-center">
-        <div className="flex items-center">
-          <input
-            placeholder="Enter Note ID"
-            value={noteID}
-            onChange={(e) => setNoteID(e.target.value)}
-            className="w-full mr-4 px-4 py-2 border rounded-lg shadow outline-none focus:ring-2 focus:ring-primary duration-200 ring-offset-2"
-          />
-          <button
-            onClick={handleGoToNote}
-            disabled={loading}
-            className="flex items-center px-4 py-2 bg-green-500 text-white rounded-lg shadow outline-none focus-visible:ring-2 focus-visible:ring-primary duration-200 ring-offset-2"
-          >
-            {loading && (
-              <FiLoader className="animate-spin mr-2" />
-            )}
-            Create
-          </button>
+      <div className="mt-8 md:mt-0 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="flex flex-col justify-center space-y-8">
+          <h1 className="text-5xl font-bold">Share Your Notes Online with Ease</h1>
+          <p className="text-2xl">Easy note-sharing. Create, edit, and share notes via URL. Simple idea collaboration!</p>
+          <div>
+            <div className="flex items-center">
+              <input
+                placeholder="Enter Note ID"
+                value={noteID}
+                onChange={(e) => setNoteID(e.target.value)}
+                className="w-full mr-4 px-4 py-2 border rounded-lg shadow outline-none focus:ring-2 focus:ring-primary duration-200 ring-offset-2"
+              />
+              <button
+                onClick={handleGoToNote}
+                disabled={loading}
+                className="flex items-center px-4 py-2 bg-green-500 text-white rounded-lg shadow outline-none focus-visible:ring-2 focus-visible:ring-primary duration-200 ring-offset-2"
+              >
+                {loading && (
+                  <FiLoader className="animate-spin mr-2" />
+                )}
+                Create
+              </button>
+            </div>
+            {errorMessage && <p className="mt-4 text-red-500">{errorMessage}</p>}
+          </div>
         </div>
-        {errorMessage && <p className="mt-4 text-red-500">{errorMessage}</p>}
+        <div>
+          <Image src="/bear.svg" alt="bear" width={100} height={100} className="w-full select-none" />
+        </div>
       </div>
-      <div>
-        <Image src="/bear.svg" alt="bear" width={100} height={100} className="w-full select-none" />
+      <div className="my-8 text-center">
+        <p>&copy; m1ngjp All Rights Reserved.</p>
       </div>
     </div>
     </>
