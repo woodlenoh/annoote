45c37da8d57132d7b34e4e102d2071ac94b64553
m1ngjp <it.m1ngjp@gmail.com>
2024-10-23 00:02:24 +0900

    Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <div className="py-8 text-center select-none opacity-50">
-            <p>&copy; 2024 Annoote.</p>
+        <div className="mt-8 border-t py-8 grid grid-cols-2 md:grid-cols-3 gap-4">
+            <div className="space-y-4">
+                <Link href="/">
+                    <Image src="/logo.svg" alt="logo" width={100} height={100} className="w-8" />
+                </Link>
+                <p>&copy; 2024 Annoote.</p>
+            </div>
+            <div className="md:hidden" />
+            <div>
+                <p className="mb-4 font-bold">Guides</p>
+                <div className="flex flex-col space-y-2">
+                    <Link href="#">How to Use</Link>
+                    <Link href="#">FAQ</Link>
+                </div>
+            </div>
+            <div>
+                <p className="mb-4 font-bold">Legal</p>
+                <div className="flex flex-col space-y-2">
+                    <Link href="#">Terms of Use</Link>
+                    <Link href="#">Privacy Policy</Link>
+                </div>
+            </div>
         </div>
     )
 }
